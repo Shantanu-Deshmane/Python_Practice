594cc75c4d24d2e247b79827c4cb51fe5b435e96
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-11-25 23:02:00 +0530

    Find GCD using OOP approach

--- 1OOP/Class.py
+++ 1OOP/Class.py
@@ -1,11 +1,23 @@
-class GCD:
-    a=int(input("Enter a first number: "))
-    b=int(input("Enter a Decond number: "))
-    while a != b :
-        if a>b:
-            a-=b
-        else:
-            b-=a
+class gcd_calculate:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def GCD(self):
+        a,b=self.num1,self.num2
+        while a!=b:
+            if a>b:
+               a-=b
+            else:
+                b-=a
+        return a
 
-result=GCD()
-print("GCD of entered numbers is: ", result.a)
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+gcdCalculate=gcd_calculate(a,b)
+gcd=gcdCalculate.GCD()
+
+print("GCD of Entetred data is: ",gcd)
+
+
+        
